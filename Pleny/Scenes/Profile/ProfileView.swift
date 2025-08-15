0da994a5d47e1b833d3123bc0f78df77fa96b1c8
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-08-15 22:19:47 +0300

    add profile tab

---
+++ Pleny/Scenes/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Pleny
+//
+//  Created by Ibrahim Abdul Aziz on 15/08/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
