50109b63e174e61505645f15f90027ef18f9f864
ibrahim3ziz <Ibrahim.aziz1198@gmail.com>
2025-08-17 00:43:50 +0300

    add constants

---
+++ Pleny/Scenes/AppCore/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Pleny
+//
+//  Created by Ibrahim Abdul Aziz on 16/08/2025.
+//
+
+
+struct Constants {
+    static let isUserLoggedIn = "isUserLoggedIn"
+    static let userSession = "UserSession"
+}
